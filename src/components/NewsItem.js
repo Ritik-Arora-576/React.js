df4fedae952db2a84feaf38faa12c7192ce0adb2
Ritik-Arora-576 <ritikarora656@gmail.com>
2023-02-15 22:27:30 +0530

    Navbar, NewsItem added (Video-23)

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        These are the news item component.
+      </div>
+    )
+  }
+}
+
+export default NewsItem
